9bcf32ac4349cbce3a6de49fef543f23544e8660
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-17 21:15:44 -0500

    Update accounts_db.php

--- accounts_db.php
+++ accounts_db.php
@@ -1,33 +1,36 @@
 <?php
+class AccountDB {
+    public static function validate_login($email, $password) {
+        global $db;
+        $query = 'SELECT * FROM accounts WHERE email = :email AND password = :password';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':password', $password);
+        $statement->execute();
+        $user = $statement->fetch();
+        $statement->closeCursor();
 
-function validate_login($email, $password) {
-	global $db;
-	$query = 'SELECT * FROM accounts WHERE email = :email AND password = :password';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':email', $email);
-	$statement->bindValue(':password', $password);
-	$statement->execute();
-	$user = $statement->fetch();
-
-	$statement->closeCursor();
-
-	if(count($user) > 0){
-		return $user['id'];
-	}else{
-		return false;
-	}
-
-} 
-function get_user($userId) {
-	global $db;
-	$query = 'SELECT * FROM accounts WHERE id = :userId';
-
-	$statement = $db->prepare($query);
-	$statement->bindValue(':userId', $userId);
-	$statement->execute();
-	$user = $statement->fetch();
-
-	$statement->closeCursor();
-
+        if (count($user) > 0) {
+        $user = new Account($user['id'], $user['email'], $user['fname'], $user['lname'], 
+            $user['birthday'], $user['password']);
+        return $user;
+    } else {
+        return false;
+    }
 }
-?>
+/*function validate_login($email, $password) {
+    global $db;
+    $query = 'SELECT * FROM accounts WHERE email = :email AND password = :password';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $password);
+    $statement->execute();
+    $user = $statement->fetch();
+    $statement->closeCursor();
+
+    if (count($user) > 0) {
+        return $user['id'];
+    } else {
+        return false;
+    }
+} */
