175d50cb6ee1333823f0aa171f7db5c0438c440a
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-17 21:14:39 -0500

    Create account.php

---
+++ account.php
@@ -0,0 +1,56 @@
+<?php
+
+class Account {
+	private $id, $email, $fname, $lname, $birthday, $password;
+
+	public function __construct($id, $email, $fname, $lname, $birthday, $password){
+		$this->id= $id;
+		$this->email = $email;
+		$this->fname = $fname;
+		$this->lname = $lname;
+		$this->birthdaay = $birthday;
+		$this->password = $password;
+
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+
+	public function getFname(){
+		return $this->fname;
+	}
+	public function setFname($fname) {
+		$this->fname = $fname;
+	}
+
+		public function getLname(){
+		return $this->lname;
+	}
+	public function setLname($lname) {
+		$this->lname = $lname;
+	}
+	public function getBirthday(){
+		return $this->birthday;
+	}
+	public function setBirthday($birthday) {
+		$this->birthday = $birthday;
+	}
+	public function getPassword(){
+		return $this->password;
+	}
+	public function setPassword($password) {
+		$this->password = $password;
+	}
+}
+
