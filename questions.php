952e4b19b78884955b08e9009e491c4a28ebd664
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-20 13:39:11 -0500

    Create questions.php

---
+++ questions.php
@@ -0,0 +1,43 @@
+<?php
+
+class Questions {
+	private $ownerid, $title, $body, $skills;
+
+	public function __construct($ownerid, $title, $body, $skills){
+		$this->ownerid= $ownerid;
+		$this->title = $title;
+		$this->body = $body;
+		$this->skills = $skills;
+
+	}
+
+	public function getOwnerid(){
+		return $this->ownerid;
+	}
+	public function setId($ownerid) {
+		$this->id = $ownerid;
+	}
+
+		public function getTitle(){
+		return $this->title;
+	}
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+		public function getBody(){
+		return $this->body;
+	}
+	public function setBody($body) {
+		$this->body = $body;
+	}
+
+		public function getSkills(){
+		return $this->skills;
+	}
+	public function setSkills($skills) {
+		$this->skills = $skills;
+	}
+}
+
+?>
