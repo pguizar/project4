8b9cc203c25f4e90004dd75973663f8ba20a5c1b
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-20 15:27:51 -0500

    Update accounts_db.php

--- accounts_db.php
+++ accounts_db.php
@@ -18,6 +18,20 @@ class AccountDB {
         return false;
     }
 }
+    public static function get_user($userId) {
+        global $db;
+        $query = 'SELECT * FROM accounts WHERE id = :userId';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':userId', $userId);
+        $statement->execute();
+        $user = $statement->fetch();
+
+        $statement->closeCursor();
+
+        $user = new Account($user['id'], $user['email'], $user['fname'], $user['lname'], $user['birthday'], $user['password']);
+
+}
 }
 /*function validate_login($email, $password) {
     global $db;
