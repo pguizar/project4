48a70fd3fe928f278670fd23e5c74411737d22b5
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-20 11:58:18 -0500

    Update accounts_db.php

--- accounts_db.php
+++ accounts_db.php
@@ -18,6 +18,7 @@ class AccountDB {
         return false;
     }
 }
+}
 /*function validate_login($email, $password) {
     global $db;
     $query = 'SELECT * FROM accounts WHERE email = :email AND password = :password';
